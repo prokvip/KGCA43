ade105029db897369fc620ec74e34fc8a13ad033
prokvip <prokvip@msn.com>
2023-10-20 13:53:51 +0900

    1020-4

--- Queue_2/Queue_2.cpp
+++ Queue_2/Queue_2.cpp
@@ -7,51 +7,12 @@ struct TQueue
     int front= 0;
 };
 TQueue g_Queue;
-void Enqueue(int data)
-{
-    g_Queue.item[++g_Queue.rear] = data;
-}
-int Dequeue()
-{
-    return g_Queue.item[g_Queue.front++];
-}
-int Size()
-{
-    return (g_Queue.rear - g_Queue.front + 1);
-}
-bool IsEmpty()
-{
-    if (g_Queue.front> g_Queue.rear) 
-    {
-        return true; 
-    }
-    return false;
-}
-bool IsFull()
-{
-    if (Size() >= MAX_SIZE)
-    {
-        return true;
-    }
-    return false;
-}
-
-void Display()
-{
-    if (!IsEmpty())
-    {
-        for (int i = g_Queue.front;
-            i <= g_Queue.rear;
-            i++)
-        {
-            std::cout << g_Queue.item[i] << std::endl;
-        }
-    }
-    else
-    {
-        std::cout << "Queue Underflow" << std::endl;
-    }
-}
+void    Enqueue(int data);
+int     Dequeue();
+int     Size();
+bool    IsEmpty();
+bool    IsFull();
+void    Display();
 
 int g_iValue = 0;
 int main()
@@ -98,4 +59,52 @@ int main()
 
     Display();
     std::cout << "Hello World!\n";
+}
+
+
+
+void Enqueue(int data)
+{
+    g_Queue.item[++g_Queue.rear] = data;
+}
+int Dequeue()
+{
+    return g_Queue.item[g_Queue.front++];
+}
+int Size()
+{
+    return (g_Queue.rear - g_Queue.front + 1);
+}
+bool IsEmpty()
+{
+    if (g_Queue.front > g_Queue.rear)
+    {
+        return true;
+    }
+    return false;
+}
+bool IsFull()
+{
+    if (Size() >= MAX_SIZE)
+    {
+        return true;
+    }
+    return false;
+}
+
+void Display()
+{
+    if (!IsEmpty())
+    {
+        for (int i = g_Queue.front;
+            i <= g_Queue.rear;
+            i++)
+        {
+            std::cout << g_Queue.item[i] << std::endl;
+        }
+    }
+    else
+    {
+        std::cout << "Queue Underflow" << std::endl;
+    }
 }
