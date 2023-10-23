5e215c7bc3d6f35f5da4bed64bc038a725eb2fc0
prokvip <prokvip@msn.com>
2023-10-23 20:56:30 +0900

    1023

---
+++ Queue_3Class/TQueue.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#define MAX_SIZE  10
+class TQueue
+{
+public:
+    int     item[MAX_SIZE];
+    int     rear = -1;
+    int     front = 0;
+public:
+    void    Enqueue(int data);
+    int     Dequeue();
+    int     Size();
+    bool    IsEmpty();
+    bool    IsFull();
+    void    Display();
+};
+
