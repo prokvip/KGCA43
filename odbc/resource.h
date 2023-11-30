7851ec3bb687d73f4a7fee10646e43e3cfb6e41d
prokvip <prokvip@msn.com>
2023-11-30 13:56:33 +0900

    1130_1

---
+++ odbc/resource.h
@@ -0,0 +1,25 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// odbc.rc에서 사용되고 있습니다.
+//
+#define IDD_DIALOG1                     101
+#define IDR_MENU1                       103
+#define IDC_NAME                        1001
+#define IDC_PRICE                       1002
+#define IDC_ITEMLIST                    1004
+#define IDC_CHECK1                      1005
+#define ID_DB_40001                     40001
+#define ID_DB_40002                     40002
+#define IDM_EXIT                        40003
+#define ID_DB_DLG                       40004
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40005
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
