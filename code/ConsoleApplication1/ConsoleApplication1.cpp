32e1aff3a6f82981d0f2fc1eada8b91f8cf0a65a
prokvip <prokvip@msn.com>
2024-04-16 15:35:59 +0900

    0416_stack

--- code/ConsoleApplication1/ConsoleApplication1.cpp
+++ code/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,20 +1,73 @@
-﻿// ConsoleApplication1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <iostream>
-
+﻿#include <stdio.h>
+// 1, 재귀호출
+int Factorial(int n);
+int g_iCounter = 0;
+void HanioTower(int n, char from, char temp, char to)
+{
+    if (n == 1)
+    {
+        printf("[%d]번 %c -> %c\n", n, from, to);
+        g_iCounter++;
+    }
+    else
+    {
+        //a를 c을 임시저장소로 활용하여 b로 놓는다.
+        HanioTower(n - 1, from, to, temp);
+        printf("[%d]번 %c -> %c\n", n, from, to);
+        g_iCounter++;
+        //b를 a을 임시저장소로 활용하여 c로 놓는다.
+        HanioTower(n - 1, temp, from, to);
+    }
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    // 3! = 3*2*1;
+    // 5! = 5*4*3*2*1;
+    int iSum = 1;
+    int n = 3;
+
+    printf("팩토리얼 결과값 = %d\n", Factorial(n));
+    
+    // 1      
+    // 2      
+    // 3    2     1
+    // 4    1     2
+    // A    B     C
+    //from temp to
+    HanioTower(3, 'A','B','C');
+
+    for (int i = n;
+        i > 0;
+        i--)
+    {
+        iSum = iSum * i;
+    }
+
+    /*for ( int i = 0; 
+          i < n; 
+          i++)
+    {
+        iSum = iSum * (i+1);
+    }*/
+    /*for ( int i = 1;
+          i <= n;
+          i++)
+    {
+        iSum = iSum * i;
+    }*/
+    printf("팩토리얼 결과값 = %d", iSum);
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+//재귀함수 버전
+int Factorial(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        int iFact = n - 1;
+        int iSum = Factorial(iFact);
+        return n * iSum;
+    }    
+}
---
+++ code/Stack/Stack.cpp
@@ -0,0 +1,71 @@
+﻿#include <stdio.h>
+#include <iostream>
+int g_iStack[5];
+int top;
+void InitStack(); // 데이터초기화
+int  push(int data);
+int  pop();
+void print();
+
+int main()
+{
+    InitStack();
+    push(1);
+    push(2);
+    int iRet = pop();
+    push(3);
+    push(4);
+    push(-1);
+    print();
+
+    push(6);
+    push(7);
+    push(8);
+
+    iRet = pop();
+    iRet = pop();
+    push(9);
+    print();
+    iRet = pop();
+    iRet = pop();
+    iRet = pop();
+}
+
+void InitStack()
+{
+
+}
+int push(int data)
+{
+    //_ASSERT(top < 5);
+    //int ni = sizeof(g_iStack) / sizeof(g_iStack[0]);
+   // int n = _countof(g_iStack);
+    if (top >= 5)
+    {
+        printf("\nStack Overflow!");        
+        printf(" %s:%d", __FUNCTION__, __LINE__);
+        return -1 ;
+    }
+    g_iStack[top++] = data; 
+    printf("\npush:%d", data);
+    return 0;
+}
+int  pop()
+{
+    //_ASSERT(top > 0);
+    if (top <= 0)
+    {
+        printf("\nStack Underflow!");
+        printf(" %s:%d", __FUNCTION__, __LINE__);
+        return -99999;
+    }
+    printf("\npop");
+    return g_iStack[--top];
+}
+void print()
+{
+    for (int i = top-1; i >= 0; i--)
+    {
+        printf("\n%d ", g_iStack[i]);
+    }
+}
