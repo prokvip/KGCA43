618f9b2a6c6cfdf8c937410ace2f293237c489d3
prokvip <prokvip@msn.com>
2024-05-23 13:03:43 +0900

    0523

--- code/CreateWindow/Sample.cpp
+++ code/CreateWindow/Sample.cpp
@@ -1,6 +1,6 @@
 //error LNK2019 : WinMain"int __cdecl invoke_main(void)" (? invoke_main@@YAHXZ) 함수에서 참조되는 확인할 수 없는 외부 기호
 #include <windows.h>
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM
+LRESULT CALLBACK AAAA(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM
 	lParam);
 
 int WINAPI wWinMain( HINSTANCE hInstance,
@@ -11,7 +11,7 @@ int WINAPI wWinMain( HINSTANCE hInstance,
 	// 1)윈도우클래스를 운영제체에 등록해야 한다.
 	// 지정된 구조체를 체워서 등록한다.
 	WNDCLASS wc = { };
-	wc.lpfnWndProc = WindowProc;	// 집주소(또는 전화번호)
+	wc.lpfnWndProc = AAAA;	// 집주소(또는 전화번호)
 	wc.hInstance = hInstance;		// 주민번호
 	wc.lpszClassName = L"홍길동2";	// 이름
 	RegisterClass(&wc);
@@ -37,8 +37,9 @@ int WINAPI wWinMain( HINSTANCE hInstance,
 	}
 	ShowWindow(hwnd, nCmdShow);
 	
-	// Run the message loop.
+	// 메세지큐에 담겨있는 모든 메세지를 수작업으로 프로시져에 전달한다.
 	MSG msg = { };
+	//WM_QUIT
 	while (GetMessage(&msg, NULL, 0, 0) > 0)
 	{
 		TranslateMessage(&msg);
@@ -47,13 +48,28 @@ int WINAPI wWinMain( HINSTANCE hInstance,
 	return 0;
 }
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM
+LRESULT CALLBACK AAAA(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM
 	lParam)
 {
 	switch (uMsg)
 	{
+	/*case WM_LBUTTONDOWN:
+	{
+		MessageBox(hwnd, L"WM_LBUTTONDOWN?",
+			L"My application", MB_OK);
+	}*/
+	//case WM_CLOSE:
+	//{
+	//	MessageBox(hwnd, L"WM_CLOSE?",
+	//		L"My application", MB_OK);
+	//}
 	case WM_DESTROY:
-		PostQuitMessage(0);
+		//if (MessageBox(hwnd, L"Really quit?", L"My application", MB_OKCANCEL) ==
+		//	IDOK)
+		//{
+		//	DestroyWindow(hwnd);
+		//}
+		PostQuitMessage(0);// WM_QUIT
 		return 0;
 	case WM_PAINT:
 	{
