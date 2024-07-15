68f65f511490c77a59b0c0061d3a9b2cb816bb90
prokvip <prokvip@msn.com>
2024-07-15 12:22:40 +0900

    0715_4

---
+++ code/net_Client_Chat/net_Client_Echo.cpp
@@ -0,0 +1,95 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <WinSock2.h>
+#include <iostream>
+#include <string>
+#pragma comment(lib,"ws2_32.lib")
+bool InitWinSock()
+{
+    //_In_ WORD wVersionRequested,
+    WSADATA WSAData;
+    int ret = WSAStartup(MAKEWORD(2, 2), &WSAData);
+    if (ret != 0) return false;
+    return true;
+}
+void DelWinSock()
+{
+    WSACleanup();
+}
+int main()
+{
+    InitWinSock();
+    SOCKET sock = socket(AF_INET, // IP
+        SOCK_STREAM,  //SOCK_DGRAM
+        IPPROTO_TCP); //,IPPROTO_TCP,IPPROTO_UDP
+
+    SOCKADDR_IN sa;    
+    //inet_pton , InetPton
+    sa.sin_addr.s_addr = inet_addr("192.168.0.12");
+    sa.sin_port = htons(10000); //0  ~ 1024
+    sa.sin_family = AF_INET;
+    int namelen = sizeof(sa);
+    int ret = connect(sock,  (sockaddr*)&sa,    namelen);
+    if (ret != 0) return 1;
+
+    // 넌블록형 소켓으로 전환
+    u_long iNonSocket = TRUE;
+    int iMode = ioctlsocket(sock, FIONBIO, &iNonSocket);
+
+   std::string UserName = "[홍길동]";
+   std::string Sendbuf;
+   Sendbuf.reserve(256);
+   std::string KeyDatabuf;
+   KeyDatabuf.reserve(256);
+
+   std::string Recvbuf;
+   Recvbuf.resize(256);
+    
+    int iSendCounter = 100;
+    while (iSendCounter-- > 0)
+    {
+        Sendbuf.clear();
+        Sendbuf = UserName;
+        std::cout << "데이터입력(종료:엔터) : ";
+        std::getline(std::cin, KeyDatabuf);
+        if (KeyDatabuf.empty())
+        {
+            break;
+        }
+        Sendbuf += KeyDatabuf;
+
+        int SendByte  = send(sock, Sendbuf.c_str(), Sendbuf.size(),0);
+        if (SendByte < 0)
+        {
+            int iError = WSAGetLastError();
+            if (iError != WSAEWOULDBLOCK)
+            {
+                break;
+            }
+            continue;
+        }
+        std::cout << "보내고" << SendByte << Sendbuf.c_str() << std::endl;
+
+        int RecvByte = recv(sock, &Recvbuf[0],Recvbuf.size(),0);
+        if (RecvByte == 0)
+        {
+            std::cout << "서버 종료!" << std::endl;
+            break;
+        }
+        if (RecvByte < 0)
+        {
+            int iError = WSAGetLastError();
+            if (iError != WSAEWOULDBLOCK)
+            {
+                std::cout << "비정상 서버 종료!" << std::endl;
+                break;
+            }
+        }
+        else
+        {
+            std::cout << "받고" << RecvByte << Recvbuf.c_str() << std::endl;
+        }
+    }
+    closesocket(sock);
+    DelWinSock();
+    std::cout << "Hello World!\n";
+}
---
+++ code/net_Server_Chat/net_Server_Echo.cpp
@@ -0,0 +1,153 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <iostream>
+#include <vector>
+#include <list>
+#pragma comment(lib,"ws2_32.lib")
+bool InitWinSock()
+{
+    WSADATA WSAData;
+    int ret = WSAStartup(MAKEWORD(2,2),&WSAData);
+    if (ret != 0) return false;
+    return true;
+}
+void DelWinSock()
+{
+    WSACleanup();
+}
+
+struct TSession
+{
+    SOCKET      sock;
+    SOCKADDR_IN addr;
+    std::string buf;
+    UINT        RecvByte;
+    TSession(SOCKET sock, SOCKADDR_IN addr)
+    {
+        RecvByte = 0;
+        this->sock = sock;
+        this->addr = addr;
+        buf.resize(256);
+    }
+};
+
+std::list<TSession> g_sockList;
+
+int main()
+{
+    InitWinSock();
+    SOCKET g_hListenSock = socket(AF_INET, SOCK_STREAM, 0);
+    SOCKADDR_IN sa;
+    ZeroMemory(&sa, sizeof(sa));
+    sa.sin_addr.s_addr = INADDR_ANY;
+    //inet_pton(AF_INET, "192.168.0.12", &sa.sin_addr.s_addr);
+    sa.sin_port = htons(10000);
+    sa.sin_family = AF_INET;
+    int namelen = sizeof(sa);
+    int ret = bind(g_hListenSock,(sockaddr*)&sa,namelen);
+    ret  = listen(g_hListenSock, SOMAXCONN);
+    // 넌블록형 소켓으로 전환
+    u_long iNonSocket = TRUE;
+    int iMode = ioctlsocket(g_hListenSock, FIONBIO, &iNonSocket);
+
+    SOCKADDR_IN addr;
+    int addrlen = sizeof(addr);    
+    int iMsgCounter = 0;
+   
+    SOCKET clientSock;
+    while (1)
+    {
+        clientSock = accept(g_hListenSock, (sockaddr*)&addr, &addrlen);
+        if (clientSock == INVALID_SOCKET)
+        {
+            int iError = WSAGetLastError();
+            if (iError != WSAEWOULDBLOCK)
+            {
+                break;
+            }
+        }
+        else
+        {
+            std::cout << "접속 ip=" <<
+                inet_ntoa(addr.sin_addr) << "port="
+                << ntohs(addr.sin_port) << std::endl;
+
+            g_sockList.emplace_back(clientSock, addr);
+        }
+
+        for( auto iter = g_sockList.begin();
+             iter != g_sockList.end();
+              )
+        {
+            TSession& session = (*iter); 
+            session.RecvByte = 0;
+
+            SOCKET sock = session.sock;
+            char buf[256] = { 0, };
+            // 받고
+            int RecvByte = recv(sock,
+                &session.buf[0],
+                session.buf.size(),
+                0);
+
+            if (RecvByte == 0)
+            {
+                closesocket(sock);
+                std::cout << "정상 접속종료 : " << inet_ntoa(addr.sin_addr) << std::endl;
+                iter = g_sockList.erase(iter);
+                continue;
+            }
+            if (RecvByte < 0)
+            {
+                int iError = WSAGetLastError();
+                if (iError != WSAEWOULDBLOCK)
+                {
+                    closesocket(sock);
+                    std::cout << "비정상 서버 종료!" << std::endl;
+                    iter = g_sockList.erase(iter); 
+                    continue;
+                }
+                else
+                {
+                    iter++;
+                    continue;
+                }
+            }
+
+            // 정상적인 데이터 수신
+            session.RecvByte = RecvByte;
+            for (auto iter = g_sockList.begin();
+                iter != g_sockList.end();
+                )
+            {
+                TSession& user = (*iter);
+                SOCKET sock = user.sock;
+                if (session.RecvByte > 0)
+                {
+                    std::cout << "받고" << session.RecvByte << session.buf.c_str() << std::endl;
+                    // 보내고
+                    int SendByte = send(sock, session.buf.c_str(), session.RecvByte, 0);
+                    if (SendByte < 0)
+                    {
+                        int iError = WSAGetLastError();
+                        if (iError != WSAEWOULDBLOCK)
+                        {
+                            closesocket(sock);
+                            std::cout << "비정상 서버 종료!" << std::endl;
+                            iter = g_sockList.erase(iter);
+                            continue;
+                        }
+                    }                    
+                }
+                iter++;
+            }            
+            
+        }        
+   }
+
+    closesocket(clientSock);
+    closesocket(g_hListenSock);
+    DelWinSock();
+    std::cout << "Hello World!\n";
+}
